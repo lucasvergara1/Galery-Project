0c2000eb91d8aef33841b8204cd3e9b4f18bdf32
Lucas <lucas.vergara@live.com>
2024-03-12 00:56:24 -0300

    :construction: Working in progress

--- app.js
+++ app.js
@@ -7,3 +7,31 @@ function getElement(selection) {
     `Please check "${selection}" selector, no such element exists`
   );
 }
+
+function Gallery(element){
+  this.container = element
+  this.list = [...element.querySelectorAll('.img')];
+ // target
+ this.modal= getElement('.modal');
+ this.modalImg = getElement('.modal-img');
+ this.modalImages = getElement('.modal-images');
+ this.closeBtn = getElement('.close-btn');
+ this.nexteBtn = getElement(".next-btn");
+ this.prevBtn = getElement(".prev-btn");
+ // bind functions
+//this.openModal = this.openModal.bind(this);
+//container event
+ this.container.addEventListener('click', function(e){
+  this.openModal();
+ }.bind(this)
+ );
+}
+
+Gallery.prototype.openModal = function(){
+
+  this.modal.classList.add('open')
+
+}
+
+const nature = new Gallery(getElement('.nature'))
+const city = new Gallery(getElement('.city'))
