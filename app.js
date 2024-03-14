bc0bb551e569e54ffded80f600d419e9cd0911ce
Lucas <lucas.vergara@live.com>
2024-03-13 23:32:32 -0300

    :construction: Next Images and Prev Images

--- app.js
+++ app.js
@@ -17,10 +17,13 @@ function Gallery(element){
  this.imageName = getElement('.image-name');
  this.modalImages = getElement('.modal-images');
  this.closeBtn = getElement('.close-btn');
- this.nexteBtn = getElement(".next-btn");
+ this.nextBtn = getElement(".next-btn");
  this.prevBtn = getElement(".prev-btn");
  // bind functions
 //this.openModal = this.openModal.bind(this);
+this.closeModal = this.closeModal.bind(this);
+this.nextImage = this.nextImage.bind(this);
+this.prevImage = this.prevImage.bind(this);
 //container event
  this.container.addEventListener('click', 
  function(e){
@@ -40,11 +43,37 @@ Gallery.prototype.openModal = function(selectedImage,list){
       "modal-img selected":"modal-img"}"/>`
   }).join('');
   this.modal.classList.add('open');
+  this.closeBtn.addEventListener('click', this.closeModal)
+  this.nextBtn.addEventListener("click", this.nextImage);
+  this.prevBtn.addEventListener("click", this.prevImage);
 };
 
 Gallery.prototype.setMainImage = function(selectedImage){
   this.modalImg.src = selectedImage.src;
   this.imageName.textContent = selectedImage.title;
+};
+
+Gallery.prototype.closeModal = function(){
+  this.modal.classList.remove('open');
+  this.closeBtn.removeEventListener("click", this.closeModal);
+  this.nextBtn.removeEventListener("click", this.nextImage);
+  this.prevBtn.removeEventListener("click", this.prevImage);
+};
+
+Gallery.prototype.nextImage = function (){
+  const selected = this.modalImages.querySelector('.selected');
+  const next = selected.nextElementSibling || this.modalImages.firstElementChild;
+  selected.classList.remove('selected');
+  next.classList.add('selected');
+  this.setMainImage(next);
+};
+Gallery.prototype.prevImage = function () {
+  const selected = this.modalImages.querySelector(".selected");
+  const prev =
+    selected.previousElementSibling || this.modalImages.lastElementChild;
+  selected.classList.remove("selected");
+  prev.classList.add("selected");
+  this.setMainImage(prev);
 };
 
 const nature = new Gallery(getElement('.nature'));
